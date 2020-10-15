f22c8bc28450e11dff4e991ded1dbe96e965c051
fyii200 <fslyii@hotmail.com>
2020-10-15 13:16:09 +0800

    201015 @ 13:15 GMT + 8

--- Code/pvt.201013.fy.r
+++ Code/pvt.201013.fy.r
@@ -1,7 +1,7 @@
-rm (list=ls()) #clear everything
-setwd ("/Users/fabianyii/Desktop/EyeQue Study/pvt/Plots") #set working "data" as working directory
+rm (list=ls())
+setwd ("/Users/fabianyii/Desktop/EyeQue Study/pvt/Plots")
 library("readxl") #attach readxl package
-eyeque <- data.frame(read_excel("/Users/fabianyii/Desktop/EyeQue Study/pvt/Data/eyeque.xlsx")) #read excel data into R
+eyeque <- data.frame(read_excel("/Users/fabianyii/Desktop/EyeQue Study/pvt/Data/eyeque.xlsx"))
 
 #define Mfunction
 mfunction <- function (s, c) {round(
@@ -18,8 +18,9 @@ eyeque$m <- mfunction (eyeque$sphere, eyeque$cyl)
 eyeque$j0 <- j0function (eyeque$cyl, eyeque$axis)
 eyeque$j45 <- j45function (eyeque$cyl, eyeque$axis)
 
-#create scatter plots: m, j0
+#create plots
 library(RColorBrewer)
+pdf (file = "/Users/fabianyii/Desktop/EyeQue Study/pvt/Plots/Rplot.pdf", width = 12, height = 8)
 
 unique <- unique(data.frame(name = eyeque$name, eye = eyeque$eye))
 for(i in 1: length(unique$name)){
@@ -32,30 +33,34 @@ for(i in 1: length(unique$name)){
   plot(d$m, d$j0, main = paste0(unique$name[i], "(",unique$eye[i],")", ":", " ", "J0, M"), 
        xlab = "M (D)", ylab = "J0 (D)", 
        xlim = c(min(d$m)-1, max(d$m)+1), ylim = c(min(d$j0)-1, max(d$j0)+1),
-       abline (h = mean(d$j0), v = mean(d$m), lty = 1, lwd = 0.3),
+       abline (h = mean(d$j0), v = mean(d$m), lty = 1, lwd = 0.15),
        pch = 19, cex = 1.2, col = brewer.pal(10, 'RdYlBu'))
   lines (d$m, d$j0, lty = 1, lwd = 0.1)
+  text(d$m, d$j0, labels = row.names(data.frame(d$name)), pos=2, cex=0.7)
   
   plot(d$m, d$j45, main = paste0(unique$name[i], "(",unique$eye[i],")", ":", " ", "J45, M"),
        xlab = "M (D)", ylab = "J45 (D)", 
        xlim = c(min(d$m)-1, max(d$m)+1), ylim = c(min(d$j45)-1, max(d$j45)+1),
-       abline (h = mean(d$j45), v = mean(d$m), lty = 1, lwd = 0.3),
+       abline (h = mean(d$j45), v = mean(d$m), lty = 1, lwd = 0.15),
        pch = 19, cex = 1.2, col = brewer.pal(10, 'RdYlBu'))
   lines (d$m, d$j45, lty = 1, lwd = 0.1)
+  text(d$m, d$j45, labels = row.names(data.frame(d$name)), pos=4, cex=0.7)
      
   plot(d$j0, d$j45, main = paste0(unique$name[i], "(",unique$eye[i],")", ":", " ", "J45, J0"),
        xlab = "J0 (D)", ylab = "J45 (D)", 
        xlim = c(min(d$j0)-1, max(d$j0)+1), ylim = c(min(d$j45)-1, max(d$j45)+1),
-       abline (h = mean(d$j45), v = mean(d$j0), lty = 1, lwd = 0.3),
+       abline (h = mean(d$j45), v = mean(d$j0), lty = 1, lwd = 0.15),
        pch = 19, cex = 1.2, col = brewer.pal(10, 'RdYlBu'))
   lines (d$j0, d$j45, lty = 1, lwd = 0.1)
+  text(d$j0, d$j45, labels = row.names(data.frame(d$name)), pos=2, cex=0.7)
   
   plot(d$sphere, d$cyl, main = paste0(unique$name[i], "(",unique$eye[i],")", ":", " ", "Cylinder, Sphere"),
        xlab = "Sphere (D)", ylab = "Cylinder (D)", 
        xlim = c(min(d$sphere)-1, max(d$sphere)+1), ylim = c(min(d$cyl)-1, max(d$cyl)+1),
-       abline (h = mean(d$cyl), v = mean(d$sphere), lty = 1, lwd = 0.3),
+       abline (h = mean(d$cyl), v = mean(d$sphere), lty = 1, lwd = 0.15),
        pch = 19, cex = 1.2, col = brewer.pal(10, 'RdYlBu'))
-  lines (d$sphere, d$cyl, lty = 1, lwd = 0.1)
+  lines (d$sphere, d$cyl, lty = 1, lwd = 0.1,)
+  text(d$sphere, d$cyl, labels = row.names(data.frame(d$name)), pos=2, cex=0.7)
   
   d$x <- 0
   d$y <- 0
@@ -72,3 +77,4 @@ for(i in 1: length(unique$name)){
   abline(a=0, b=d$slope[9], lty=1, lwd=1, col="red")
   abline(a=0, b=d$slope[10], lty=1, lwd=1, col="red")
 }
+dev.off()
